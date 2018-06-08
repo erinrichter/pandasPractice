59c97f0a03f693cdebb912fd7c6bc24f34eb7d78
Lawrence Weikum <lawrencemq@gmail.com>
2018-06-08 09:09:56 -0700

    Added football-game data, added Python (3) requirements for environment, and added the script to read in the football data and print the average score any team scores in a game. More stats to follow.

---
+++ scores.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+
+# Reading in all football games and scores
+df = pd.read_csv('football_games.csv')
+#print(df)
+
+
+########### Questions to follow #############
+
+# Find the average score any team has ever made
+df1 = pd.concat([df['home_team_points'], df['away_team_points']])
+#print(df1)
+print(df1.mean())
+
+# Find the average score of each team
+
+# Find the average score of each team by year
+
+# Find the team with the highest score ever made
+
+# Find the team with the most points scored overall
+
+# Find how many games have ended in a tie
+
+# Show how many points were scored in total by year
